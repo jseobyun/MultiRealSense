cc4b1e5ed61383cc3e6b7dccbef9c11e352515a0
jseob.y <jseob.y@naverlabs.com>
2021-09-07 15:07:18 +0900

    3view managing

--- capture_3view.py
+++ capture_3view.py
@@ -1,29 +1,22 @@
 import pyrealsense2 as rs
 import cv2
 import os
+import json
 from pynput.keyboard import Listener
 import argparse
-import numpy as np
-import time
+from view3_manager import View3Manager
 
-from collections import defaultdict
-from device_manager import DeviceManager
 def parse_config():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--exp_name', default='test ')
+    parser.add_argument('--exp_name', default='default')
     args = parser.parse_args()
     return args
 
 def capture(root):
-
     # Define some constants
     L515_resolution_width = 1024  # pixels
     L515_resolution_height = 768  # pixels
     L515_frame_rate = 30
-
-    resolution_width = 1024  # 1280 # pixels
-    resolution_height = 768  # 720 # pixels
-    frame_rate = 30  # fps
 
     rgb_width = 1920
     rgb_height = 1080
@@ -33,24 +26,24 @@ def capture(root):
         L515_rs_config = rs.config()
         L515_rs_config.enable_stream(rs.stream.depth, L515_resolution_width, L515_resolution_height, rs.format.z16, L515_frame_rate)
         L515_rs_config.enable_stream(rs.stream.infrared, L515_resolution_width, L515_resolution_height, rs.format.y8, L515_frame_rate)
-        L515_rs_config.enable_stream(rs.stream.color, rgb_width, rgb_height, rs.format.bgr8, frame_rate)
+        L515_rs_config.enable_stream(rs.stream.color, rgb_width, rgb_height, rs.format.bgr8, L515_frame_rate)
 
-        rs_config = rs.config()
-        rs_config.enable_stream(rs.stream.depth, resolution_width, resolution_height, rs.format.z16, frame_rate)
-        rs_config.enable_stream(rs.stream.infrared,  resolution_width, resolution_height, rs.format.y8, frame_rate)
-        rs_config.enable_stream(rs.stream.color, rgb_width, rgb_height, rs.format.bgr8, frame_rate)
 
         # Use the device manager class to enable the devices and get the frames
-        device_manager = DeviceManager(rs.context(), rs_config, L515_rs_config)
+        device_manager = View3Manager(rs.context(), L515_rs_config, L515_rs_config, L515_rs_config)
         device_manager.enable_all_devices()
         def on_press(key):
             if str(key) == "Key.space":
                 frames = device_manager.get_frames(root, save=True)
+
+                intrinsic = device_manager.get_device_intrinsics(frames)
+                with open(os.path.join(root, 'intrinsic.json'), 'w') as json_file:
+                    json.dump(intrinsic, json_file)
                 print("Current frame is caputred")
             else:
                 frames = device_manager.get_frames(root, save=False)
 
-        print("Waiting") 
+        print("Waiting")
         with Listener(on_press = on_press) as listener:
             listener.join()
 
@@ -71,7 +64,9 @@ if __name__ == "__main__":
         os.mkdir(os.path.join(root, 'images'))
         os.mkdir(os.path.join(root, 'images', 'view0'))
         os.mkdir(os.path.join(root, 'images', 'view1'))
+        os.mkdir(os.path.join(root, 'images', 'view2'))
         os.mkdir(os.path.join(root, 'depths'))
         os.mkdir(os.path.join(root, 'depths', 'view0'))
         os.mkdir(os.path.join(root, 'depths', 'view1'))
+        os.mkdir(os.path.join(root, 'depths', 'view2'))
     capture(root)
